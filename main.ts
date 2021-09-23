6835c23d6d64a3aa8812a1268cad8b5c15065f0d
GollmerHenrik <78141464+GollmerHenrik@users.noreply.github.com>
2021-09-23 12:33:16 +0200

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,57 @@
+let dobás = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        dobás = randint(0, 6)
+    }
+})
+basic.forever(function () {
+    if (dobás == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (dobás == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (dobás == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (dobás == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (dobás == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (dobás == 6) {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+    }
+})
